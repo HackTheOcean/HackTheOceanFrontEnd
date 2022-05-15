ce52438a506b774720c8023336b8b9555e46aeab
Julieta Delgado <julidmeza@gmail.com>
2022-05-15 00:24:18 -0500

    Change path images Pollution impact

--- src/components/PollutingImpact.vue
+++ src/components/PollutingImpact.vue
@@ -34,7 +34,7 @@
                 </div>
             </div>
             <div class="col-5 text-center justify-content-center align-self-center">
-                <img src="@/assets/img/turtle-polluting-impact.png" class="img-fluid" id="image-polluting-impact">
+                <img src="@/assets/img/polluting-impact/turtle-polluting-impact.png" class="img-fluid" id="image-polluting-impact">
                 <p class="fw-normal">{{ NegativeEffectPollutingImpact }}</p>
                 <p id="text-polluting-impact">Artículos como sogas, redes, trampas y líneas de monofilamento de equipos de pesca abandonados, perdidos o descartados enmarañan a los animales marinos, causándoles heridas, asfixia, restricciones de movilidad y la muerte. </p>
             </div>
@@ -54,23 +54,23 @@ export default {
         onChange(event) {
             var data = event.target.value;
             if(data == 'Enredos'){
-                document.getElementById('image-polluting-impact').src = require('@/assets/img/turtle-polluting-impact.png')
+                document.getElementById('image-polluting-impact').src = require('@/assets/img/polluting-impact/turtle-polluting-impact.png')
                 document.getElementById('text-polluting-impact').textContent = 'Artículos como sogas, redes, trampas y líneas de monofilamento de equipos de pesca abandonados, perdidos o descartados enmarañan a los animales marinos, causándoles heridas, asfixia, restricciones de movilidad y la muerte.'
             }
             else if(data == 'Ingesta'){
-                document.getElementById('image-polluting-impact').src = require('@/assets/img/seagull-polluting-impact.png')
+                document.getElementById('image-polluting-impact').src = require('@/assets/img/polluting-impact/seagull-polluting-impact.png')
                 document.getElementById('text-polluting-impact').textContent = 'Los animales marinos de todo tipo ingieren plástico, desde los superpredadores hasta el plancton en la base de la cadena alimenticia. Esto puede ocasionarles lesiones graves, afectando la ingesta de alimentos al generar un falso sentido de saciedad, bloqueando su sistema digestivo o causando heridas internas'
             }
             else if(data == 'Asfixia'){
-                document.getElementById('image-polluting-impact').src = require('@/assets/img/seal-polluting-impact.png')
+                document.getElementById('image-polluting-impact').src = require('@/assets/img/polluting-impact/seal-polluting-impact.png')
                 document.getElementById('text-polluting-impact').textContent = 'La contaminación por plásticos priva de luz, alimentos y oxígeno a los corales, esponjas y animales que viven en el lecho marino y reduce la cantidad de oxígeno y comida disponible en los sedimentos. Esto puede impactar negativamente a los ecosistemas y darle cabida a los patógenos en deteriorode la vida marina'
             }
             else if(data == 'Contaminación química'){
-                document.getElementById('image-polluting-impact').src = require('@/assets/img/chemical-polluting-impact.png')
+                document.getElementById('image-polluting-impact').src = require('@/assets/img/polluting-impact/chemical-polluting-impact.png')
                 document.getElementById('text-polluting-impact').textContent = 'Aunque no todos los ingredientes en el plástico son dañinos, algunos sí lo son y pueden filtrarse en el ambiente marino. Las partículas de plástico más pequeñas pueden atravesar las células de los animales marinos y algunas pueden llegar hasta el cerebro.'
             }
             else{
-                document.getElementById('image-polluting-impact').src = require('@/assets/img/turtle-polluting-impact.png')
+                document.getElementById('image-polluting-impact').src = require('@/assets/img/polluting-impact/turtle-polluting-impact.png')
                 document.getElementById('text-polluting-impact').textContent = 'Selecciona el efecto negativo y conoce su impacto'
             }
         }
