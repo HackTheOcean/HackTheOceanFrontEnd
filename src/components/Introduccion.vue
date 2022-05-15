7732aab0126741275ff3560d936af8a173639d4c
PaolaMPrado <leonmx7@hotmail.com>
2022-05-15 16:44:49 -0500

    Menu modificado e introduccion sin css

---
+++ src/components/Introduccion.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Impacto ambiental</h1>
+
+    <p>Los estragos que el ser humano a generado con la manipulación de los recursos naturales y la sinterización de estos ha provocado un sin número de anomalías que repercuten en el ambiente. <br>Desde que comenzamos a construir cuidades hemos enviado todos nuestros desechos a los mares y ríos lo que a lo largo de los años ah provocado que en la actualidad existan grandes islas de basura, la más grande de ellas se encuentra en el pacífico con 710 000 km² y 17 000 000 km² lo cual ya rebasa el tamaño de muchos países.</p>
+
+    <img src="https://i.imgur.com/T8KWpnY.jpeg" alt="Isla de basura" width="350px" height="350px">
+
+    <img src="https://i.imgur.com/E1Wr2Ck.png" alt="Isla de basura" width="350px" height="350px">
+
+    <p>Esto afecta a la fauna marina que a su vez afectan al ser humano como lo son el consumo de micro plásticos consumidos por animales que posteriormente serán consumidos por el ser humano. Mucha de la flora marina también ayuda a la reducción del Co2 en el ambiente y al ser destruida también envenenamos nuestro aire. </p>
+
+    <img src="https://i.imgur.com/i55OFPi.jpeg" alt="Isla de basura" width="350px" height="350px">
+
+    <img src="https://i.imgur.com/fbIBezz.jpeg" alt="Isla de basura" width="350px" height="350px">
+
+     
+
+   <br> <a href="https://www.youtube.com/watch?v=MnCbTTTi7ic" target="blank">Garbage Patch</a>
+
+   <p>El cambio está en nosotros, el planeta es de todos.</p>
+    
+
+</template>
+<script></script>
+<style> 
+</style>
