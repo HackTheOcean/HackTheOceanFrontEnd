8e1d065d9279f03cabcf0b8a9adcae859831d375
Julieta Delgado <julidmeza@gmail.com>
2022-05-14 22:52:38 -0500

    Add onchange event for negative effect input

--- src/components/PollutingImpact.vue
+++ src/components/PollutingImpact.vue
@@ -9,25 +9,25 @@
             <div class="offset-1 col-5 filter-negative-effect justify-content-center align-self-center">
                 <p>Selecciona el efecto negativo y conoce su impacto</p>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="negative-effect" id="Enredos" checked>
+                    <input class="form-check-input" type="radio" name="negative-effect" id="Enredos"  v-model="PollutingImpactText" value="Enredos" checked @change="onChange($event)">
                     <label class="form-check-label" for="Enredos">
                         Enredos
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="negative-effect" id="Ingesta">
+                    <input class="form-check-input" type="radio" name="negative-effect" id="Ingesta" v-model="PollutingImpactText" value="Ingesta" @change="onChange($event)">
                     <label class="form-check-label" for="Ingesta">
                         Ingesta
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="negative-effect" id="Asfixia">
+                    <input class="form-check-input" type="radio" name="negative-effect" id="Asfixia" v-model="PollutingImpactText" value="Asfixia" @change="onChange($event)">
                     <label class="form-check-label" for="Asfixia">
                         Asfixia
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="negative-effect" id="chemicalContamination">
+                    <input class="form-check-input" type="radio" name="negative-effect" id="chemicalContamination" v-model="PollutingImpactText" value="Contaminación química" @change="onChange($event)">
                     <label class="form-check-label" for="chemicalContamination">
                         Contaminación química
                     </label>
@@ -35,6 +35,7 @@
             </div>
             <div class="col-5 text-center justify-content-center align-self-center">
                 <img src="@/assets/img/turtle-polluting-impact.jpg" class="img-fluid" id="image-polluting-impact">
+                <p>{{ PollutingImpactText }}</p>
                 <p id="text-polluting-impact">Artículos como sogas, redes, trampas y líneas de monofilamento de equipos de pesca abandonados, perdidos o descartados enmarañan a los animales marinos, causándoles heridas, asfixia, restricciones de movilidad y la muerte. </p>
             </div>
         </div>
@@ -43,7 +44,18 @@
 
 <script>
 export default {
-  name: 'PollutingImpact'
+    name: 'PollutingImpact',
+    data() {
+        return{
+            PollutingImpactText: 'Enredos'
+        }               // initialize the_answer to be your checked option
+    },
+    methods:{
+        onChange(event) {
+            var data = event.target.value;
+            console.log(data);
+        }
+    }
 }
 </script>
 
